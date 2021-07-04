ed26d9b7d80c0e744f38db1eb85e5aa6830573db
Fazri Anggriawan <fazri.anggriawan@gmail.com>
2021-07-04 12:26:34 +0700

    test post method

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,20 +10,20 @@ class LoginController extends BaseController
 {
     public function dologin(Request $request)
     {
-        $request->input('username');
-        $request->input('password');
-        $mod = new Login();
-        $data = $mod->getLogin()
-            ->where('username', $request->input('username'))
-            ->where('password', $request->input('password'))
-            ->all();
-        if( count($data) == 1 ){
-            $jwt = getJWT($data[0]->username,$data[0]->password);
-            $array = array('auth'=>true,'token'=>$jwt, 'role'=>$data[0]->role, 'id_pelaksana'=>$data[0]->id_pelaksana);
-        }else{
-            $array = array('auth'=>false,'token'=>null, 'role'=>null);
-        }
-        return $array;
+//        $request->input('username');
+//        $request->input('password');
+//        $mod = new Login();
+//        $data = $mod->getLogin()
+//            ->where('username', $request->input('username'))
+//            ->where('password', $request->input('password'))
+//            ->all();
+//        if( count($data) == 1 ){
+//            $jwt = getJWT($data[0]->username,$data[0]->password);
+//            $array = array('auth'=>true,'token'=>$jwt, 'role'=>$data[0]->role, 'id_pelaksana'=>$data[0]->id_pelaksana);
+//        }else{
+//            $array = array('auth'=>false,'token'=>null, 'role'=>null);
+//        }
+//        return $array;
     }
 
     public function aa(){
