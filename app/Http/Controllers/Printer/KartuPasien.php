5deac5e04c3479b93fe06952e38bb6a994688761
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2022-11-13 13:40:50 +0700

    set margin top

--- app/Http/Controllers/Printer/KartuPasien.php
+++ app/Http/Controllers/Printer/KartuPasien.php
@@ -36,7 +36,7 @@ class KartuPasien extends BaseController
 		$pdf = new PDFBarcode();
         $pdf->SetAutoPageBreak(false);
         $pdf->AddPage('P', [110,110], 0);
-        $pdf->SetXY(13, 20);
+        $pdf->SetXY(13, 23);
         $pdf->SetMargins(13,0);
 
         $header = new HeaderPrint();
