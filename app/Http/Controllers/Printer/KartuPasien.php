37d59a8b064320638c11e0a383605ae6abb43c94
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2022-11-13 13:21:37 +0700

    tambah nomor rm dan rubah paper size

--- app/Http/Controllers/Printer/KartuPasien.php
+++ app/Http/Controllers/Printer/KartuPasien.php
@@ -35,8 +35,8 @@ class KartuPasien extends BaseController
 
 		$pdf = new PDFBarcode();
         $pdf->SetAutoPageBreak(false);
-        $pdf->AddPage('P', [60,90], 0);
-        $pdf->SetXY(13, 13);
+        $pdf->AddPage('P', [110,110], 0);
+        $pdf->SetXY(13, 25);
         $pdf->SetMargins(13,0);
 
         $header = new HeaderPrint();
