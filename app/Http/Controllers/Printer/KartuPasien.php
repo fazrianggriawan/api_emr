606cbf285513ae6c93d085614f0017145a269ba6
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2022-11-13 13:28:13 +0700

    setting margin

--- app/Http/Controllers/Printer/KartuPasien.php
+++ app/Http/Controllers/Printer/KartuPasien.php
@@ -30,19 +30,25 @@ class KartuPasien extends BaseController
         header("Content-type:application/pdf");
 
 		$border = 0;
-		$heightCell = 3;
+		$heightCell = 2;
 		$widthCell = 50;
 
 		$pdf = new PDFBarcode();
         $pdf->SetAutoPageBreak(false);
         $pdf->AddPage('P', [110,110], 0);
-        $pdf->SetXY(13, 25);
+        $pdf->SetXY(13, 23);
         $pdf->SetMargins(13,0);
 
         $header = new HeaderPrint();
         $setting = $header->GetSetting( new stdClass() );
         $pdf->SetFont('arial', $setting->fontWeight, $setting->fontSize);
 
+        $pdf->Cell($widthCell-27, $heightCell+2, 'No. RM', $border);
+        $pdf->Cell(3, $heightCell+2, ':', $border);
+        $pdf->SetFont('arial', $setting->fontWeight, $setting->fontSize+2);
+		$pdf->Cell($widthCell, $heightCell+2, strtoupper($data->norm), $border);
+        $pdf->SetFont('arial', $setting->fontWeight, $setting->fontSize);
+        $pdf->ln();
         $pdf->Cell($widthCell-27, $heightCell+2, 'Nama Pasien', $border);
         $pdf->Cell(3, $heightCell+2, ':', $border);
 		$pdf->Cell($widthCell, $heightCell+2, substr(strtoupper($data->nama),0,24), $border);
