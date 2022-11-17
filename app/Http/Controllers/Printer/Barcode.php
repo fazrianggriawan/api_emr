b57af18f4f1868e98eee802a48f53b61f30beda8
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2022-11-17 10:25:58 +0700

    rubah alamat jadi nik diprint out barcode

--- app/Http/Controllers/Printer/Barcode.php
+++ app/Http/Controllers/Printer/Barcode.php
@@ -52,7 +52,7 @@ class Barcode extends BaseController
         $heightCell++;
         $pdf->Cell($widthCell, $heightCell, $tglLahir->format('d-m-Y').', '.$jnsKelamin, $border);
 		$pdf->ln();
-		$pdf->Cell($widthCell, $heightCell, substr(strtoupper($data->alamat),0,24), $border);
+		$pdf->Cell($widthCell, $heightCell, 'NIK : '.@$data->nik, $border);
 		$pdf->SetFont('arial', '', $fontBody);
 		$pdf->Code128( $marginLeft+1.3, 15, $data->norm, 40, 9); // Barcode
 		$pdf->SetFont('arial', $fontWeight, $fontBody);
