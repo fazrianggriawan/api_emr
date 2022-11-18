02a39012089d6e22e94bfa26ffd71126ba368f4f
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2022-11-18 19:37:45 +0700

    rubah method cari obat farmasi dari GET ke POST

--- app/Http/Controllers/Farmasi/ObatController.php
+++ app/Http/Controllers/Farmasi/ObatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Farmasi;
 
 use App\Http\Libraries\LibApp;
 use App\Models\Simrs_lama\Apotik_salak_obatx;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class ObatController extends BaseController
@@ -15,10 +16,10 @@ class ObatController extends BaseController
         return LibApp::response(200, $data);
     }
 
-    public function CariObat($key)
+    public function CariObat(Request $request)
     {
         $data = Apotik_salak_obatx::select(['kode as id', 'nama','hrg_jual1 as harga','pak2 as satuan','sr_recno as id_tarif_harga'])
-                ->where('nama', 'like', strtoupper($key).'%')
+                ->where('nama', 'like', '%'.strtoupper($request->keyword).'%')
                 ->where('sr_deleted', '')
                 ->orderBy('nama')
                 ->get();
